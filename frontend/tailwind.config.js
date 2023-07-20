b6d405c3ab52e3f62d5b5ec05f33c31fff836b9f
Jumzer <bluevictor@hotmail.fr>
2023-07-20 15:14:14 +0200

    font change done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["LightTalouFont"],
+      heading: ["BoldTalouFont"],
+    },
     backgroundImage: {
       home_bg: "url('./src/assets/images/R2.jpg')",
     },
