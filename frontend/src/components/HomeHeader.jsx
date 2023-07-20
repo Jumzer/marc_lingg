1984cc6448ae36b557cd054fc04ab832b4193351
Jumzer <bluevictor@hotmail.fr>
2023-07-20 14:26:32 +0200

    fix header

--- frontend/src/components/HomeHeader.jsx
+++ frontend/src/components/HomeHeader.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo/logo_1_white.png";
 
 function Header() {
   return (
-    <div className="bg-black">
+    <div className="myHeaderHomeBackground">
       <div className="flex flex-row justify-between">
         <div className="flex items-center basis-1/2">
           <h1 className="ml-2 p-2 text-center text-2xl">Marc Lingg</h1>
@@ -12,6 +12,15 @@ function Header() {
         <div className="basis-1/2 flex justify-end p-4">
           <img className="w-10" src={logo} alt="logo world" />
         </div>
+      </div>
+      <div className="flex flex-row">
+        <div className="ml-2 p-2 basis-1/2">
+          <h3>Bagnolet</h3>
+          <h3>lucio@photo.fr</h3>
+          <h3>06 05 04 03 02</h3>
+          <h3>Instagram</h3>
+        </div>
+        <div className="basis-1/2" />
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import Header from "@components/Header";
+import HomeHeader from "@components/HomeHeader";
 import FooterHome from "@components/FooterHome";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-home_bg bg-cover flex">
       <div className="flex flex-col w-full">
-        <Header />
+        <HomeHeader />
         <div className="flex flex-1 items-end">
           <FooterHome />
         </div>
