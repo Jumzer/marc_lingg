cba6a91ae5e6320651a7adc82300a95075285a69
Jumzer <bluevictor@hotmail.fr>
2023-07-20 14:15:22 +0200

    fixing homeheader

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,24 @@ import logo from "../assets/logo/logo_1_white.png";
 
 function Header() {
   return (
-    <div className="myBackground flex flex-row justify-between">
-      <div className="flex items-center basis-1/2">
-        <h1 className="ml-2 p-2 text-center text-2xl">Marc Lingg</h1>
+    <div className="myHeaderHomeBackground">
+      <div className="flex flex-row justify-between">
+        <div className="flex items-center basis-1/2">
+          <h1 className="ml-2 p-2 text-center text-2xl">Marc Lingg</h1>
+        </div>
+
+        <div className="basis-1/2 flex justify-end p-4">
+          <img className="w-10" src={logo} alt="logo world" />
+        </div>
       </div>
-      <div className="basis-1/2 flex justify-end p-4">
-        <img className="w-10" src={logo} alt="logo world" />
+      <div className="flex flex-row">
+        <div className="ml-2 p-2 basis-1/2">
+          <h3>Bagnolet</h3>
+          <h3>lucio@photo.fr</h3>
+          <h3>06 05 04 03 02</h3>
+          <h3>Instagram</h3>
+        </div>
+        <div className="basis-1/2" />
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Header from "@components/Header";
 import FooterHome from "@components/FooterHome";
-import Infos from "@components/Infos";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-home_bg bg-cover flex">
       <div className="flex flex-col w-full">
         <Header />
-        <Infos />
         <div className="flex flex-1 items-end">
           <FooterHome />
         </div>
